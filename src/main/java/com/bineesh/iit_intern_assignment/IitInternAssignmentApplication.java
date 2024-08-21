7b7f0e2e61e9c88e67df28017fa52531888a0c5c
Bineesh-pro <bineeshprofessional@gmail.com>
2024-08-21 23:06:49 +0530

    backend project intial setup

---
+++ src/main/java/com/bineesh/iit_intern_assignment/IitInternAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.bineesh.iit_intern_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IitInternAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IitInternAssignmentApplication.class, args);
+    }
+
+}
